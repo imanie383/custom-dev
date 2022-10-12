523360b2d814966782fb8d445f4930c2c2e47324
LuisMzz <luis.manuel@vauxoo.com>
2022-10-12 14:40:38 -0500

    [IMP] open_academy: set version

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Open Academy",
 
@@ -17,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '15.0.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
--- open_academy/models/partner.py
+++ open_academy/models/partner.py
@@ -2,7 +2,6 @@ from odoo import models, fields
 
 
 class Partner(models.Model):
-    _name = 'res.partner'
     _inherit = 'res.partner'
 
     instructor = fields.Boolean()
