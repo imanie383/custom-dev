6ee36a833e9353e455e3383e6caf1efea8580333
LuisMzz <luis.manuel@vauxoo.com>
2022-10-12 10:35:16 -0500

    [ADD] open_academy: categories
    Exercise 14: Add teacher categories and update session domain
    to display partners with the category teacher

--- open_academy/models/partner.py
+++ open_academy/models/partner.py
@@ -6,3 +6,4 @@ class Partner(models.Model):
 
     instructor = fields.Boolean()
     sessions = fields.Many2many('open_academy.session', 'instructor')
+    category = fields.Selection([('a', 'Teacher/Level 1'), ('b', 'Teacher/Level 2'),])
