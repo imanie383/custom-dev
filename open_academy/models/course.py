ea6d02fbf7e048c9a0f55e29d959ff8062dd72d5
LuisMzz <luis.manuel@vauxoo.com>
2022-10-12 16:12:49 -0500

    [ADD] open_academy: sql constraits
    Exercise 19: Add sql constraits to title
    and description in the course model

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -4,6 +4,10 @@ from odoo import models, fields
 class Course(models.Model):
     _name = 'open_academy.course'
     _description = 'Course model'
+    _sql_constraints = [
+        ('title-description-diff', 'CHECK(title != description)', 'The description and title must be different'),
+        ('title-unique', 'UNIQUE(title)', 'The title must be unique'),
+    ]
 
     title = fields.Char(required=True)
     description = fields.Text()
