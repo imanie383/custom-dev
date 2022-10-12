2a19a74001b3c8ae6928692f7bc98b99a2e847fe
LuisMzz <luis.manuel@vauxoo.com>
2022-10-12 11:43:18 -0500

    [ADD] open_academy: taken seats percentage
    Exercise 15: Add a computed field with the taken seats
    percentage, and set views to show taken_seats field
    with progressbar widget

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Session(models.Model):
@@ -9,7 +9,19 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float()
     seats = fields.Integer()
+    taken_seats = fields.Float(compute='_compute_taken_seats')
 
     instructor = fields.Many2one('res.partner', domain="['|', ('instructor', '=', 'True'), ('category', '!=', False)]")
     course = fields.Many2one('open_academy.course')
     attendees = fields.Many2many('res.partner')
+
+    @api.depends('seats', 'attendees')
+    def _compute_taken_seats(self):
+        for record in self:
+            attendees = len(record.attendees)
+            seats = record.seats
+            if attendees >= seats or seats == 0:
+                percentage = 100
+            else:
+                percentage = (100 * attendees) / seats
+            record.taken_seats = percentage
