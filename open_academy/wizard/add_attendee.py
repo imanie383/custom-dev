56bcedd8d1d4eb4340296ebd665821d55ffffbc4
LuisMzz <luis.manuel@vauxoo.com>
2022-10-15 13:52:07 -0500

    [IMP] open_academy: add_attende view
    Exercise 31: Define a form and an action for the
    wizard, and set default session to current sessión

--- open_academy/wizard/add_attendee.py
+++ open_academy/wizard/add_attendee.py
@@ -5,5 +5,8 @@ class AddAttendee(models.TransientModel):
     _name = 'open_academy.session.add_attendee'
     _description = 'Wizard model for add attedees to session'
 
-    session_id = fields.Many2one('open_academy.session')
+    session_id = fields.Many2one('open_academy.session', default=lambda self: self._get_session_id())
     attendee_ids = fields.Many2many('res.partner')
+
+    def _get_session_id(self):
+        return self._context.get('active_id')
