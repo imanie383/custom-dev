859d7221f4c609ac68a3458c7a480d379e606cb2
LuisMzz <luis.manuel@vauxoo.com>
2022-10-15 14:59:26 -0500

    [IMP] open_academy: multiple sessions
    Exercise 33: Modify wizard so that attendees can
    register to multiple sessions

--- open_academy/wizard/add_attendee.py
+++ open_academy/wizard/add_attendee.py
@@ -5,12 +5,12 @@ class AddAttendee(models.TransientModel):
     _name = 'open_academy.session.add_attendee'
     _description = 'Wizard model for add attedees to session'
 
-    session_id = fields.Many2one('open_academy.session', default=lambda self: self._get_session_id())
+    session_ids = fields.Many2many('open_academy.session', default=lambda self: self._get_session_ids())
     attendee_ids = fields.Many2many('res.partner')
 
-    def _get_session_id(self):
-        return self._context.get('active_id')
+    def _get_session_ids(self):
+        return self._context.get('active_ids')
 
     def add_attendee(self):
-        session = self.session_id
-        session.attendee_ids |= self.attendee_ids
+        for session in self.session_ids:
+            session.attendee_ids |= self.attendee_ids
