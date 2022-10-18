5f346611386c71a53e43c04e1609925b9e3124bf
LuisMzz <luis.manuel@vauxoo.com>
2022-10-18 12:58:56 -0500

    [ADD] open_academy: dashboard form
    Exercise 36: Add a dashboard form

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '15.0.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
